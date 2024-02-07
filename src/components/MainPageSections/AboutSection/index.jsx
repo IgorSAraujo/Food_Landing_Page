200ce2710f9b1cf3dece9cbc5ea436242b540400
Igor Silva <firetuskbr1@gmail.com>
2024-02-07 13:26:51 -0300

    AboutSection Finalizada

--- src/components/MainPageSections/AboutSection/index.jsx
+++ src/components/MainPageSections/AboutSection/index.jsx
@@ -1,7 +1,28 @@
+import styles from "./styles.module.scss"
+import mobileIcon from "../../../assets/MobileIcon.svg"
 export const AboutSection = () => {
   return (
-    <section>
-     
+    <section className={styles.container}>
+      <div className={styles.containerTitle}>
+        <img src={mobileIcon} alt="Icone de Celular" />
+        <h1 className="title1">
+          Sobre o <span className="textS">aplicativo</span>
+        </h1>
+      </div>
+      <div className={styles.containerText}>
+        <p className="textP">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin maximus
+          tempor ullamcorper. Vivamus porta lorem eu quam ultricies, non
+          imperdiet erat elementum. Lorem ipsum dolor sit amet, consectetur
+          adipiscing elit. Proin maximus tempor ullamcorper. Vivamus porta lorem
+          eu quam ultricies, non imperdiet erat elementum. Lorem ipsum dolor sit
+          amet, consectetur adipiscing elit. Proin maximus tempor ullamcorper.
+          Vivamus porta lorem eu quam ultricies, non imperdiet erat elementum.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin maximus
+          tempor ullamcorper. Vivamus porta lorem eu quam ultricies, non
+          imperdiet erat elementum.
+        </p>
+      </div>
     </section>
   )
 }
--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -1,12 +1,14 @@
 import { DefautTemplate } from "../../components/DefautTemplate"
 import { BannerSection } from "../../components/MainPageSections/BannerSection"
 import { RestaurantsSection } from "../../components/MainPageSections/RestaurantsSection"
+import { AboutSection } from "../../components/MainPageSections/AboutSection"
 export const MainPage = () => {
   return (
     <>
       <DefautTemplate>
         <BannerSection />
         <RestaurantsSection />
+        <AboutSection />
       </DefautTemplate>
     </>
   )
